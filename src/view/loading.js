5364915b5e15e26e77556539d2a55d21b0c54cf6
AwSicker <vitja1234@gmail.com>
2020-09-27 23:23:15 +0300

    Fix

--- src/view/loading.js
+++ src/view/loading.js
@@ -2,7 +2,7 @@ import Abstract from "../view/abstract.js";
 
 const createNofilmsTemplate = () => {
   return `<p class="films__no-films">
-  Loading.... </p>`;
+  Loading...</p>`;
 };
 
 export default class Loading extends Abstract {
